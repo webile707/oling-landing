6f0932d158aeb2ab6d14184255e3fc11288b5cb3
webile707 <qinghan910@gmail.com>
2023-06-09 10:39:58 +1000

    change firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,34 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=0;var c1=0;var c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);var c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-        apiKey: "AIzaSyAAGsIiWG3CGfbnTsUK2NEUV1-V3xbHjuA",
-        authDomain: "oling-52ee9.firebaseapp.com",
-        projectId: "oling-52ee9",
-        storageBucket: "oling-52ee9.appspot.com",
-        messagingSenderId: "177145394629",
-        appId: "1:177145394629:web:76d76459f1b0a9739887ac"
+        apiKey: "QUl6YVN5QmkzN0RDS3diNEJSNWs1OEVIbEFTY0Fkb1l3RVJkSFow",
+        authDomain: "b2xpbmctNDZjZDMuZmlyZWJhc2VhcHAuY29t",
+        projectId: "b2xpbmctNDZjZDM=",
+        storageBucket: "b2xpbmctNDZjZDMuYXBwc3BvdC5jb20=",
+        messagingSenderId: "ODc4MDA0ODk3NjM0",
+        appId: "MTo4NzgwMDQ4OTc2MzQ6d2ViOjY1NGQ4NjMxYmI1YzExZmFjMzhlM2I=",
+        measurementId: "Ry05SkY5SE1KRjdD"
 };
 
 //keys are test and do not work. you need to use a secret manager as repo is public
+for (const key in firebaseConfig) {
+        if (Object.hasOwnProperty.call(firebaseConfig, key)) {
+                const element = firebaseConfig[key];
+                firebaseConfig[key] = Base64.decode(element);
+        }
+}
 
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firestore
 const db = getFirestore(app);
